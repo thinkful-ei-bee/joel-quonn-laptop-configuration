c07b4b9e450c84af5539f6148f768587c59aac1d
joelcorey <joelcorey@fastmail.com>
2019-03-22 08:32:25 -0700

    SpecsProcessor update

--- src/component/SpecsProcessor/SpecsProcessor.js
+++ src/component/SpecsProcessor/SpecsProcessor.js
@@ -5,47 +5,35 @@ export default class SpecsProcessor extends Component {
   render() {
     console.log(this.props.processors)
 
-    const features = Object.keys(this.props.processors)
-      .map(key => {
-        const processors = this.props.processors.map((processor, index) => {
-          
-          const selectedClass = processor.name === this.props.processors.name ? 'feature__selected' : '';
-          const featureClass = 'feature__option ' + selectedClass;
+    const processors = this.props.processors.map((processor, index) => {
+      
+      const selectedClass = processor.name === this.props.processors.name ? 'feature__selected' : '';
+      const featureClass = 'feature__option ' + selectedClass;
 
-          console.log(selectedClass);
+      //console.log(selectedClass);
 
-          return (
-            <li key={index} className="feature__item">
-              <div 
-                className={featureClass}
-                onClick={e => this.props.handleUpdateProcessor(key, processor)}
-              >
-                { processor.name }
-                ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                  .format(processor.cost) })
-              </div>
-            </li>
-          )
-        });
+      return (
+        <li key={index} className="feature__item">
+          <div 
+            className={featureClass}
+            onClick={e => this.props.handleUpdateProcessor(0, processor)}
+          >
+            { processor.name }
+            ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
+              .format(processor.cost) })
+          </div>
+        </li>
+      )
+    });
 
-        return (
-          <>
-            
-              { processors }
-           
-          </>
-        )
-
-      });
-    
-    console.log(features);
+    console.log(processors);
 
     return (
       <>
         <div className="feature">Processor</div>
         <div className="feature__name"></div>
         <ul className="feature__list">
-        { features }
+        { processors }
         </ul>
       </>
     )
