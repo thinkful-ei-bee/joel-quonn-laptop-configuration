5996b95f043d0e6835ee0705184de26cc10057d1
joelcorey <joelcorey@fastmail.com>
2019-03-22 04:40:46 -0700

    init

---
+++ src/component/Header/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+export default class Header extends Component {
+
+  render() {
+    return(
+      <header>
+        <h1>ELF Computing</h1>
+        <h3>Laptops</h3>
+        <h5>Customize your laptop</h5>  
+      </header>
+    )
+  }
+}
