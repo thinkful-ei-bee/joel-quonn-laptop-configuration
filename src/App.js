ae2f978c6221cbf0ff14b67225eeca5a4d0956d8
joelcorey <joelcorey@fastmail.com>
2019-03-22 05:04:28 -0700

    remove summary const from app.js render

--- src/App.js
+++ src/App.js
@@ -37,15 +37,6 @@ class App extends Component {
   }
 
   render() {
-    const summary = Object.keys(this.state.selected)
-          .map(key => <div className="summary__option" key={key}>
-            <div className="summary__option__label">{key}  </div>
-            <div className="summary__option__value">{this.state.selected[key].name}</div>
-            <div className="summary__option__cost">
-              { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                  .format(this.state.selected[key].cost) }
-            </div>
-        </div>)
 
     const total = Object.keys(this.state.selected)
           .reduce((acc, curr) => acc + this.state.selected[curr].cost, 0);    
